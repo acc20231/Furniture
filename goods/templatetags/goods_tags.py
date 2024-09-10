e717f3bcd6b50162074c78bdc8e75fa07be8b1c4
acc20231 <alexander-9090@inbox.ru>
2024-09-10 23:26:28 +0400

    register simple tag for categories

---
+++ goods/templatetags/goods_tags.py
@@ -0,0 +1,14 @@
+from importlib import simple
+from goods.models import Categories
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def tag_categories():
+    return Categories.objects.all()
+
+
+
+
+
